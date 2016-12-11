74c3cae0ce04e4f84e36632699325889da075cdf
Developing <Developing@jerome-hadorn.local>
2016-12-11 10:20:04 +0100

    Initial Commit

---
+++ KlassenbuchTests/KlassenbuchTests.swift
@@ -0,0 +1,36 @@
+//
+//  KlassenbuchTests.swift
+//  KlassenbuchTests
+//
+//  Created by Developing on 11.12.16.
+//  Copyright © 2016 Hadorn Developing. All rights reserved.
+//
+
+import XCTest
+@testable import Klassenbuch
+
+class KlassenbuchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
