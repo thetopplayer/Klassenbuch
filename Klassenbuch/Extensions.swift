4c344e38008d4eb96d5dee9071d9d400b298e303
Developing <Developing@jerome-hadorn.local>
2017-02-04 15:26:39 +0100

    Rate App Function

--- Klassenbuch/Extensions.swift
+++ Klassenbuch/Extensions.swift
@@ -33,7 +33,7 @@ extension UIViewController {
             }
         }
 
-    
+       
     
     }
 }
@@ -47,6 +47,9 @@ extension UIViewController {
  
 }
  */
+
+
+
 
 
 
--- Klassenbuch/Informationen.swift
+++ Klassenbuch/Informationen.swift
@@ -9,16 +9,20 @@
 import UIKit
 import Firebase
 import FirebaseDatabase
+import MessageUI
 
-class Informationen: UITableViewController {
+class Informationen: UITableViewController, MFMailComposeViewControllerDelegate {
 
-   
+    
     
     //Outlets
+    @IBOutlet var AppStoreView: UIView!
   
     @IBOutlet weak var ConnectionState: UIBarButtonItem!
 
-    
+    // Variables
+    var effect:UIVisualEffect!
+
     
     
     override func viewDidLoad() {
@@ -90,4 +94,95 @@ class Informationen: UITableViewController {
     }
 
     
+    
+    
+    @IBAction func AppStoreReview(_ sender: Any) {
+        
+
+        
+            print("Rate Us row tapped.")
+            
+            let alertController = UIAlertController(title: "Bewerte die Klassenbuch App!", message: "\nAre you enjoying our app? Please rate us in the app store!\n\nElse if you know of ways we can make our app better, please send us feedback so we can improve the experience for you!\n\nThanks!\nThe App Team", preferredStyle: .alert)
+            
+            alertController.addAction(UIAlertAction(title: "Rate on iTunes", style: .default, handler: { (action: UIAlertAction!) in
+                print("RateUs.RateUs_Tapped")
+                print("Send to iTunes")
+                
+                if let path = URL(string: "https://itunes.apple.com/us/app/calcfast/id876781417?mt=8") {
+                    UIApplication.shared.open(path) {
+                        (didOpen:Bool) in
+                        if !didOpen {
+                            print("Error opening:\(path.absoluteString)")
+                        }
+                    }
+                }
+            }))
+            
+        
+            alertController.addAction(UIAlertAction(title: "Feedback", style: .default, handler: { (action: UIAlertAction!) in
+                
+                let mailComposeViewController = self.configuredMailComposeViewController()
+                
+                if MFMailComposeViewController.canSendMail() {
+                    self.present(mailComposeViewController, animated: true, completion: nil)
+                } else {
+                    self.showSendMailErrorAlert()
+                }
+
+                
+                print("RateUs.Feedback_Tapped")
+            }))
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (action: UIAlertAction!) in
+            print("RateUs.Cancel_Tapped")
+        }))
+        
+            present(alertController, animated: true, completion: nil)
+            
+        }
+
+// Mail Compose for Feedback Functions
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["klassenbuchteam@gmail.com"])
+        mailComposerVC.setSubject("Klassenbuch App Feedback")
+        mailComposerVC.setMessageBody("Hi Team!\n\nHier ist mein Feedback für die Klassenbuch App..\n", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        
+        let sendMailErrorAlert = UIAlertController(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", preferredStyle: .alert)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        sendMailErrorAlert.addAction(defaultAction)
+        
+        self.present(sendMailErrorAlert, animated: true, completion: nil)
+        
+        
+    }
+    func mailComposeController(_ controller: MFMailComposeViewController,
+                               didFinishWith result: MFMailComposeResult, error: Error?) {
+        switch result.rawValue {
+        case MFMailComposeResult.cancelled.rawValue:
+            print("Mail cancelled")
+        case MFMailComposeResult.saved.rawValue:
+            print("Mail saved")
+        case MFMailComposeResult.sent.rawValue:
+            print("Mail sent")
+        case MFMailComposeResult.failed.rawValue:
+            print("Mail sent failure: %@", [error!.localizedDescription])
+        default:
+            break
+        }
+        // Dismiss the mail compose view controller.
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
 }
+
+
